82efca307514b1e03d690f3966979c30360ee479
Steavens <steavens2010@aol.com>
2025-01-27 15:54:07 +0400

    mini changement

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './controllers/auth_controller.dart';
+import './services/api_service.dart';
+import './views/login_page.dart';
+import './views/recherche_view.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final apiService = ApiService();
+  final authController = AuthController(apiService);
+
+  await authController.loadToken();
+
+  runApp(
+    ChangeNotifierProvider<AuthController>.value(
+      value: authController,
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -9,3 +29,13 @@ class MyApp extends StatelessWidget {
     final authController = Provider.of<AuthController>(context);
 
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Gestion des Lots',
+      initialRoute: authController.token != null ? '/recherche' : '/login',
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/recherche': (context) => const RecherchePage(),
+      },
+    );
+  }
+}
